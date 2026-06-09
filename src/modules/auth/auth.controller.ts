3181b70c89b7a4711150e5b09ef1c866e4b7fd06
Azimxon <axewarred@gmail.com>
2026-06-09 08:09:51 +0500

    .

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,11 +2,11 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dtos/register.dto';
 
-@Controller()
+@Controller("auth")
 export class AuthController {
   constructor(private readonly service: AuthService) {}
 
-  @Post()
+  @Post("register")
   async register(@Body() payload: RegisterDto) {
     return await this.service.register(payload);
   }
